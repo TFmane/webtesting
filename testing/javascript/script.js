9c887be1c3e553b938cee90d364623b1f6aeb8e3
Emanuel <tfmanemail@gmail.com>
2018-06-06 17:57:23 +0200

    photo slideshow
    testing out an image slideshow

---
+++ testing/javascript/script.js
@@ -0,0 +1,58 @@
+
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+	showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+	showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+	var I;
+	var slides = document.getElementsByClassName("mySlides");
+	var dots = document.getElementsByClassName("dot");
+	if (n > slides.length) {slideIndex = 1}
+	if (n < 1) {slideIndex = slides.length}
+	for (i = 0; i < slides.length; i++) {
+		slides[i].style.display = "none";
+	}
+	for (i = 0; i < dots.length; i++) {
+		dots[i].className = dots[i].className.replace(" activedot", "");
+	}
+	slides[slideIndex-1].style.display = "block";
+	dots[slideIndex-1].className += " activedot";
+}
+/*
+$(window).ready(function(function plusSlides(n){
+	showSlides(slideIndex += n);
+	
+});
+
+$(window).ready(function(function currentSlide(n){
+	showSlides(slideIndex = n);
+	
+});
+
+$(window).ready(function(function showSlides(n){
+	
+	var I;
+	var slides = document.getElementsByClassName("mySlides");
+	var dots = $(".dot").var(dots);document.getElementsByClassName("dot");
+	if ($(n > slides.length)) {slideIndex = 1}
+	if ($(n < 1)) {slideIndex = slides.length}
+	for ($(i = 0; i < slides.length; i++)) {
+		slides[i].style.display = "none";
+	}
+	for ($(i = 0; i < dots.length; i++)) {
+		dots[i].className = dots[i].className.replace(" activedot", "");
+	}
+	slides[slideIndex-1].style.display = "block";
+	dots[slideIndex-1].className += " activedot";
+	
+});*/
